dcafbfad8d072b6bcb4aff915bf2e4d516d08cd3
ArturBanasik <32513228+ArturBanasik@users.noreply.github.com>
2018-05-28 17:43:53 +0200

    Divided Class

--- Library.java
+++ Library.java
@@ -2,22 +2,25 @@ public class Library {
 
 	public static void main (String[] args){
 
-	final String appName = "Library v0.1";
+	final String appName = "Library v0.2";
 
-	String title = "Thinking In Java";
-	String author = "Bruce Eckel";
-	int releaseDate = 2010;
-	int pages = 1213;
-	String publisher = "Helion";
-	String isbn = "9788328334427";
+
+	Book bookObject = new Book();
+
+	bookObject.title = "Thinking In Java";
+	bookObject.author = "Bruce Eckel";
+	bookObject.releaseDate = 2010;
+	bookObject.pages = 1213;
+	bookObject.publisher = "Helion";
+	bookObject.isbn = "9788328334427";
 
 	System.out.println(appName);
 	System.out.println("Books in the library: ");
-	System.out.println(title);
-	System.out.println(author);
-	System.out.println(releaseDate);
-	System.out.println(pages);
-	System.out.println(publisher);
-	System.out.println(isbn);
+	System.out.println(bookObject.title);
+	System.out.println(bookObject.author);
+	System.out.println(bookObject.releaseDate);
+	System.out.println(bookObject.pages);
+	System.out.println(bookObject.publisher);
+	System.out.println(bookObject.isbn);
 	}
 }
